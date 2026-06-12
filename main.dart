ede7c6d256a3e3c195ef481c704a54ac25421a61
Alexxdiazz <alexisdiaz2.a@gmail.com>
2026-06-11 21:34:07 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,15 +1,75 @@
-import 'package:crud/pag1.dart';
 import 'package:flutter/material.dart';
+import 'pag1.dart';
+import 'pag2.dart';
+import 'pag3.dart';
 
 void main() {
-  runApp(app());
+  runApp(const App());
 }
 
-class app extends StatelessWidget {
-  const app({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "CRUD Estudiante", home: pag1());
+    return const MaterialApp(
+      home: MenuPrincipal(),
+    );
+  }
+}
+
+class MenuPrincipal extends StatelessWidget {
+  const MenuPrincipal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("CRUD Universidade"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            ElevatedButton(
+              child: const Text("Estudantes"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const pag1(),
+                  ),
+                );
+              },
+            ),
+
+            ElevatedButton(
+              child: const Text("Disciplinas"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Pag2(),
+                  ),
+                );
+              },
+            ),
+
+            ElevatedButton(
+              child: const Text("Cursando"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Pag3(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
